e580b88e81229acb80c082b02f864b52827967fd
Sarah Naveed <114023598+SarahNk1-Maker@users.noreply.github.com>
2024-06-17 13:16:54 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -9,16 +9,36 @@ int main(int argc, char** argv) {
         fprintf(stderr, "please provide the address of the file as an input.\n");
         return -1;
     }
-     char cmd[BUFSIZE];
-    snprintf(cmd, sizeof(cmd), "wc -c < %s", argv[1]);
 
-    // Validate that the input does not contain any characters that could lead to command injection
+    // Validate that the input file name is safe
     if (strpbrk(argv[1], "&;`'\"|*?~<>^()[]{}$\\") != NULL) {
         fprintf(stderr, "Invalid characters in input.\n");
         return -1;
     }
-    system(cmd);
 
+    // Construct the command using snprintf for safety
+    char cmd[BUFSIZE];
+    snprintf(cmd, sizeof(cmd), "wc -c < %s", argv[1]);
+
+    // Use popen to safely execute the command and read the output
+    FILE *fp;
+    char result[BUFSIZE];
+
+    if ((fp = popen(cmd, "r")) == NULL) {
+        fprintf(stderr, "Error opening pipe.\n");
+        return -1;
+    }
+
+    // Read the output of the command
+    if (fgets(result, sizeof(result), fp) != NULL) {
+        printf("%s", result);
+    } else {
+        fprintf(stderr, "Error reading result.\n");
+        pclose(fp);
+        return -1;
+    }
+
+    // Close the pipe
+    pclose(fp);
     return 0;
 }
-
