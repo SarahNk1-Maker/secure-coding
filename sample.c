8af1e1e520bf89804278d9f271053ec552fe56e7
Sarah Naveed <114023598+SarahNk1-Maker@users.noreply.github.com>
2024-06-17 12:48:45 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,18 +1,19 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define BUFSIZE 256
 
-int main(int argc,char** argv) {
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "please provide the address of the file as an input.\n");
+        return -1;
+    }
+    char cmd[BUFSIZE] = "wc -c < ";
 
-if (argc != 2) {
-  fprintf(stderr,"please provide the address of the file as an input.\n");
-return -1;
+    strcat(cmd, argv[1]);
+    system(cmd);
 
+    return 0;
 }
-char cmd[BUFSIZE] = "wc -c <";
 
-strcat(cmd,argc[1]);
-system(cmd);
-}
